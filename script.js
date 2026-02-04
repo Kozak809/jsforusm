98168c1111446421b00dc030ec1e9b15c08d1604
Kozak809 <kozak800009@gmail.com>
2026-02-04 12:13:00 +0200

    lab1

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("Этот код выполнен из внешнего файла!");
+console.log("Сообщение в консоли");
+
+let name = "Student"; 
+let birthYear = 2000;
+let isStudent = false;
+
+console.log("Name:", name);
+console.log("Birth Year:", birthYear);
+console.log("Is Student:", isStudent);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
