2e93c21607c5f7b22f3166271383d042c51e9ddc
Kozak809 <kozak800009@gmail.com>
2026-02-11 12:24:35 +0200

    feat(LAB1): added LAB_REPORT.md

--- lab1/script.js
+++ lab1/script.js
@@ -10,12 +10,18 @@ console.log("Birth Year:", birthYear);
 console.log("Is Student:", isStudent);
 
 let score = prompt("Введите ваш балл:");
-if (score >= 90) {
- console.log("Отлично!");
-} else if (score >= 70) {
- console.log("Хорошо");
+
+if (score === null || score.trim() === "" || isNaN(score)) {
+  console.log("Ошибка: Введено не число или пустая строка!");
 } else {
- console.log("Можно лучше!");
+  score = Number(score);
+  if (score >= 90) {
+    console.log("Отлично!");
+  } else if (score >= 70) {
+    console.log("Хорошо");
+  } else {
+    console.log("Можно лучше!");
+  }
 }
 
 for (let i = 1; i <= 5; i++) {
